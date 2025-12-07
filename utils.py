c9fd1879d93d41c7f22d59398ac3951e0995aa3c
houcine1amraoui <houcine1amraoui@gmail.com>
2025-12-07 16:27:25 +0100

    up

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def height(node):
 def balance_factor(node):
     if node is None: return 0
     return height(node.left) - height(node.right)
+
+def print_bst(root, level=0, prefix="Root: "):
+    if root is not None:
+        print("    " * level + prefix + str(root.value))
+        print_bst(root.left, level + 1, "L--- ")
+        print_bst(root.right, level + 1, "R--- ")
